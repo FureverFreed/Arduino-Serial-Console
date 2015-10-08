ff8ce1d9aafd1b692bbf168c622242dedb876fc0
ajfreed00 <ajfreed00@gmail.com>
2015-10-08 16:01:52 -0500

    New setpin command
    You can now set pins' states by running setpin in the serial console.
    You will be guided through a menu to choose which pin, and what state
    you would like to set it to. 0, 1, and 13 pins cannot have their state
    set.

--- src/Version 1.0/upload/upload.ino
+++ src/Version 1.0/upload/upload.ino
@@ -36,6 +36,177 @@ void fatal(String error){
     delay(500);
   }
 }
+void setPin(String x){
+  if (x=="0\n"){
+    Serial.println("Invalid pin, serial communication only.");
+  }
+  if (x=="1\n"){
+    Serial.println("Invalid pin, serial communication only.");
+  }
+  if (x=="2\n"){
+    Serial.println("Regular digital pin, on or off?");
+    digitalWrite(ledPin, HIGH);
+    while(not Serial.available()){
+      // do nothing.
+    }
+    String state = Serial.readString();
+    digitalWrite(ledPin, LOW);
+    pinMode(2, OUTPUT);
+    if (state=="on\n"){
+      digitalWrite(2, HIGH);
+    }
+    if (state=="off\n"){
+      digitalWrite(2, LOW);
+    }
+  }
+  if (x=="3\n"){
+    Serial.println("Regular digital pin, on or off?");
+    digitalWrite(ledPin, HIGH);
+    while(not Serial.available()){
+      // do nothing.
+    }
+    String state = Serial.readString();
+    digitalWrite(ledPin, LOW);
+    pinMode(3, OUTPUT);
+    if (state=="on\n"){
+      digitalWrite(3, HIGH);
+    }
+    if (state=="off\n"){
+      digitalWrite(3, LOW);
+    }
+  }
+  if (x=="4\n"){
+    Serial.println("Regular digital pin, on or off?");
+    digitalWrite(ledPin, HIGH);
+    while(not Serial.available()){
+      // do nothing.
+    }
+    String state = Serial.readString();
+    digitalWrite(ledPin, LOW);
+    pinMode(4, OUTPUT);
+    if (state=="on\n"){
+      digitalWrite(4, HIGH);
+    }
+    if (state=="off\n"){
+      digitalWrite(4, LOW);
+    }
+  }
+  if (x=="5\n"){
+    Serial.println("Regular digital pin, on or off?");
+    digitalWrite(ledPin, HIGH);
+    while(not Serial.available()){
+      // do nothing.
+    }
+    String state = Serial.readString();
+    digitalWrite(ledPin, LOW);
+    pinMode(5, OUTPUT);
+    if (state=="on\n"){
+      digitalWrite(5, HIGH);
+    }
+    if (state=="off\n"){
+      digitalWrite(5, LOW);
+    }
+  }
+  if (x=="6\n"){
+    Serial.println("Regular digital pin, on or off?");
+    digitalWrite(ledPin, HIGH);
+    while(not Serial.available()){
+      // do nothing.
+    }
+    String state = Serial.readString();
+    digitalWrite(ledPin, LOW);
+    pinMode(6, OUTPUT);
+    if (state=="on\n"){
+      digitalWrite(6, HIGH);
+    }
+    if (state=="off\n"){
+      digitalWrite(6, LOW);
+    }
+  }
+  if (x=="7\n"){
+    Serial.println("Regular digital pin, on or off?");
+    digitalWrite(ledPin, HIGH);
+    while(not Serial.available()){
+      // do nothing.
+    }
+    String state = Serial.readString();
+    digitalWrite(ledPin, LOW);
+    pinMode(7, OUTPUT);
+    if (state=="on\n"){
+      digitalWrite(7, HIGH);
+    }
+    if (state=="off\n"){
+      digitalWrite(7, LOW);
+    }
+  }
+  if (x=="9\n"){
+    Serial.println("Regular digital pin, on or off?");
+    digitalWrite(ledPin, HIGH);
+    while(not Serial.available()){
+      // do nothing.
+    }
+    String state = Serial.readString();
+    digitalWrite(ledPin, LOW);
+    pinMode(9, OUTPUT);
+    if (state=="on\n"){
+      digitalWrite(9, HIGH);
+    }
+    if (state=="off\n"){
+      digitalWrite(9, LOW);
+    }
+  }
+  if (x=="10\n"){
+    Serial.println("Regular digital pin, on or off?");
+    digitalWrite(ledPin, HIGH);
+    while(not Serial.available()){
+      // do nothing.
+    }
+    String state = Serial.readString();
+    digitalWrite(ledPin, LOW);
+    pinMode(10, OUTPUT);
+    if (state=="on\n"){
+      digitalWrite(10, HIGH);
+    }
+    if (state=="off\n"){
+      digitalWrite(10, LOW);
+    }
+  }
+  if (x=="11\n"){
+    Serial.println("Regular digital pin, on or off?");
+    digitalWrite(ledPin, HIGH);
+    while(not Serial.available()){
+      // do nothing.
+    }
+    String state = Serial.readString();
+    digitalWrite(ledPin, LOW);
+    pinMode(11, OUTPUT);
+    if (state=="on\n"){
+      digitalWrite(11, HIGH);
+    }
+    if (state=="off\n"){
+      digitalWrite(11, LOW);
+    }
+  }
+  if (x=="12\n"){
+    Serial.println("Regular digital pin, on or off?");
+    digitalWrite(ledPin, HIGH);
+    while(not Serial.available()){
+      // do nothing.
+    }
+    String state = Serial.readString();
+    digitalWrite(ledPin, LOW);
+    pinMode(12, OUTPUT);
+    if (state=="on\n"){
+      digitalWrite(12, HIGH);
+    }
+    if (state=="off\n"){
+      digitalWrite(12, LOW);
+    }
+  }
+  if (x=="13\n"){
+    Serial.println("Invalid pin, built-in basic communication LED.");
+  }
+}
 void loop() {
   // put your main code here, to run repeatedly:
   Serial.println();
@@ -49,14 +220,33 @@ void loop() {
   Serial.println("Type help for a help menu, or exit to freeze the system until reset.");
   Serial.print(">>> ");
   while(true){
+    digitalWrite(ledPin, HIGH);
     if(Serial.available()){
       String input = Serial.readString();
+      digitalWrite(ledPin, LOW);
       if(input=="exit\n"){
         fatal("System exit.");
       }
       if(input=="help\n"){
         Serial.println("Console Help");
-        Serial.println("This console is run by Termite-3.2, please do not use other software.");
+        Serial.println("This console is designed for Termite-3.2, please do not use other software.");
+        Serial.println("Command list:");
+        Serial.println("  help - Shows this help menu.");
+        Serial.println("  exit - Freezes the Arduino.");
+        Serial.println("  setpin - Sets a pin to a specified state.");
+      }
+      if(input=="setpin\n"){
+        Serial.print("Type output pin: ");
+        digitalWrite(ledPin, HIGH);
+        while(not Serial.available()){
+          // do nothing.
+        }
+        String pin = Serial.readString();
+        digitalWrite(ledPin, LOW);
+        Serial.println("You can set a pin to off to use it as a ground,");
+        Serial.println("but this is not recommended. Use at your own risk.");
+        // find out type of pin and take action.
+        setPin(pin);
       }
       Serial.print(">>> ");
     }
